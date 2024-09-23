aca9ba5cf3cf1f14428b0dfc91ed6c6748a74d3a
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-09-23 16:09:49 +0700

    feat: add user service

--- modules/user/service/user_service.go
+++ modules/user/service/user_service.go
@@ -1,14 +1,62 @@
 package service
 
 import (
+	"context"
+	"log"
 	"xyz-auth-service/common/config"
+	commonErr "xyz-auth-service/common/error"
+	"xyz-auth-service/modules/user/entity"
 	"xyz-auth-service/modules/user/internal/repository"
 )
 
 type UserService struct {
-	cfg config.Config
+	cfg            config.Config
 	userRepository repository.UserRepositoryUseCase
 }
 
 type UserServiceUseCase interface {
-	
+	FindByEmail(ctx context.Context, email string) (*entity.User, error)
+	FindById(ctx context.Context, uuid string) (*entity.User, error)
+	Create(ctx context.Context, email, password string, role uint32) (*entity.User, error)
+}
+
+func NewUserService(cfg config.Config, userRepository repository.UserRepositoryUseCase) *UserService {
+	return &UserService{
+		cfg:            cfg,
+		userRepository: userRepository,
+	}
+}
+
+func (svc *UserService) FindByEmail(ctx context.Context, email string) (*entity.User, error) {
+	res, err := svc.userRepository.FindByEmail(ctx, email)
+	if err != nil {
+		parseError := commonErr.ParseError(err)
+		log.Println("ERROR: [UserService - FindByEmail] Error while find user by email: ", parseError.Message)
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (svc *UserService) FindById(ctx context.Context, uuid string) (*entity.User, error) {
+	res, err := svc.userRepository.FindById(ctx, uuid)
+	if err != nil {
+		parseError := commonErr.ParseError(err)
+		log.Println("ERROR: [UserService - FindById] Error while find user by ID: ", parseError.Message)
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (svc *UserService) Create(ctx context.Context, email, password string, role uint32) (*entity.User, error) {
+	user := entity.NewUserEntity(email, password, role)
+	res, err := svc.userRepository.Create(ctx, user)
+	if err != nil {
+		parseError := commonErr.ParseError(err)
+		log.Println("ERROR: [UserService - Create] Error while create user: ", parseError.Message)
+		return nil, err
+	}
+
+	return res, nil
+}
